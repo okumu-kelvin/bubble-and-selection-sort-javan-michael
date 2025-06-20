efed8910f810165ab2b1617a2a9573985dfc68af
Michael Javan Owino Obunga <michael.obunga@strathmore.edu>
2025-06-20 11:49:46 +0300

    Update selection_sort.py

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,3 +1,20 @@
 def selection_sort(arr):
-    # TODO: Implement selection sort
-    pass
+    # Creates a copy to preserve the original list
+    unsorted = arr.copy()
+    n = len(unsorted)
+
+    # Loop through the entire list "n" times
+    for i in range(n):
+        # Assumes that the current index has the minimum value
+        min_index = i
+
+        # Looks for a smaller value in the remaining unsorted part
+        for j in range(i + 1, n):
+            if unsorted[j] < unsorted[min_index]:
+                min_index = j
+
+        # Swaps the found minimum value with the value at the current index
+        unsorted[i], unsorted[min_index] = unsorted[min_index], unsorted[i]
+
+    # Returns the sorted list
+    return unsorted
