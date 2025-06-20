82e64425fb80042c0ef9c88d2a24d1e169820040
Michael Javan Owino Obunga <michael.obunga@strathmore.edu>
2025-06-20 11:45:12 +0300

    Update bubble_sort.py

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,16 +1,16 @@
 def bubble_sort(unsorted_list):
-    # It Creates a copy of the list to avoid modifying the original data
+    # It creates a copy of the list and does not modify the data
     arr = unsorted_list.copy()
     n = len(arr)
 
-    # Outer loop runs n times to ensure the entire list is sorted
+    # The outer loop will run "n" times to make sure list is sorted
     for i in range(n):
         # Inner loop compares adjacent elements and "bubbles" the largest to the end
         for j in range(0, n - i - 1):
-            # If the current element is greater than the next, swap them
+            # If the current element is greater than the next they are swapped
             if arr[j] > arr[j + 1]:
                 arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
-    # Return the sorted copy of the list
+    # Returns the sorted copy of the list
     return arr
 
